0437b15e6157c2c0d3f58af5672c75a9faf5da2a
Michal Garvey <michalgarvey@gmail.com>
2018-01-27 17:55:04 +0100

    adding comments to blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107134051) do
+ActiveRecord::Schema.define(version: 20180127155012) do
 
   create_table "articles", force: :cascade do |t|
     t.string "Title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20180107134051) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "rating"
+    t.integer "articles_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["articles_id"], name: "index_comments_on_articles_id"
   end
 
 end
